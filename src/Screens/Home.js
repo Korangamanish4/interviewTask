f1fe3a286810ce6c91c51abf0962b56a9fee1010
Korangamanish4 <98011226+Korangamanish4@users.noreply.github.com>
2022-11-11 03:59:31 -0800

    Unwanted code removed

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -2,16 +2,6 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import './Common.css'
 
-const customStyles = {
-    content: {
-      top: '50%',
-      left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-      transform: 'translate(-50%, -50%)',
-    },
-  };
-
 const Home = () => {
 
 const navigate = useNavigate()
